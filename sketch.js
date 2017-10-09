28aba3549f73d7675400a84b03e9e4a1b217be94
Alex Smith <alexandergeorgesmith@gmail.com>
2017-10-09 20:27:41 +0100

    Circle Illusion

--- sketch.js
+++ sketch.js
@@ -5,5 +5,14 @@ function setup() {
 }
 
 function draw() {
-	
+	drawCircle(this.width / 2, this.height / 2, mouseX);
+}
+
+function drawCircle(x, y, radius) {
+	ellipse(x, y, radius);
+
+	// if(radius > 5){
+	// 	drawCircle(x + .5, y, radius);
+	// }
+	// drawCircle();
 }
